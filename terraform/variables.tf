16a7e403a15dc1c5ffab815ae3d2b615f3ac96ef
puthiye <38553241+puthiye@users.noreply.github.com>
2020-09-17 20:34:33 +1000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,5 @@
-variable "server_port" {
-  description = "The port the web server will be listening"
-  type        = number
-  default     = 8080
-}
-
 variable "elb_port" {
-  description = "The port the elb will be listening"
+  description = "The SSH port"
   type        = number
-  default     = 80
+  default     = 22
 } 
