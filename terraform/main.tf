3de0e09b28f85cb1b5654feefd0c317a722f3fb1
puthiye <38553241+puthiye@users.noreply.github.com>
2020-09-17 20:34:01 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,33 +4,18 @@ provider "aws" {
 
 data "aws_availability_zones" "all" {}
 
-resource "aws_launch_configuration" "asg-launch-config-sample" {
+resource "aws_launch_configuration" "launch-config-sample" {
   image_id          = "ami-0810abbfb78d37cdf"
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.busybox.id]
-  
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, Terraform & AWS ASG" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
+  security_groups = [aws_security_group.elb-sg-sample.id]
+   
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_security_group" "busybox" {
-  name = "terraform-busybox-sg"
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "elb-sg" {
-  name = "terraform-sample-elb-sg"
+resource "aws_security_group" "elb-sg-sample" {
+  name = "elb-sg-demo"
   # Allow all outbound
   egress {
     from_port   = 0
@@ -38,7 +23,7 @@ resource "aws_security_group" "elb-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # Inbound HTTP from anywhere
+  # Inbound SSH from any ip
   ingress {
     from_port   = var.elb_port
     to_port     = var.elb_port
@@ -48,40 +33,38 @@ resource "aws_security_group" "elb-sg" {
 }
 
 resource "aws_autoscaling_group" "asg-sample" {
-  launch_configuration = aws_launch_configuration.asg-launch-config-sample.id
+  launch_configuration = aws_launch_configuration.launch-config-sample.id
   availability_zones   = data.aws_availability_zones.all.names
-  min_size = 2
-  max_size = 5
-  desired_capacity = 2
+  min_size = 1
+  max_size = 2
+  desired_capacity = 1
 
-  load_balancers    = [aws_elb.sample.name]
+  load_balancers    = [aws_elb.elb-sample.name]
   health_check_type = "ELB"
 
   tag {
     key                 = "Name"
-    value               = "terraform-asg-sample"
+    value               = "demo-asg"
     propagate_at_launch = true
   }
 }
 
-resource "aws_elb" "sample" {
-  name               = "terraform-asg-sample"
-  security_groups    = [aws_security_group.elb-sg.id]
+resource "aws_elb" "elb-sample" {
+  name               = "elb-demo"
+  security_groups    = [aws_security_group.elb-sg-sample.id]
   availability_zones = data.aws_availability_zones.all.names
-
+  
   health_check {
-    target              = "HTTP:${var.server_port}/"
-    interval            = 30
-    timeout             = 3
     healthy_threshold   = 2
     unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:22"
+    interval            = 15
   }
 
-  # Adding a listener for incoming HTTP requests.
+  # Adding a listener for SSH 
   listener {
     lb_port           = var.elb_port
-    lb_protocol       = "http"
-    instance_port     = var.server_port
-    instance_protocol = "http"
+    lb_protocol       = "tcp"
   }
 }    
