3e0824e4c74f086eba8801d73bf542127df98b4c
puthiye <38553241+puthiye@users.noreply.github.com>
2020-09-17 19:54:04 +1000

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "server_port" {
+  description = "The port the web server will be listening"
+  type        = number
+  default     = 8080
+}
+
+variable "elb_port" {
+  description = "The port the elb will be listening"
+  type        = number
+  default     = 80
+} 
